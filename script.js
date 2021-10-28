bcdc67e279c910f6bd9568326f192a87989f89f2
Vale2197 <valentinatrebbi97@gmail.com>
2021-10-28 15:36:28 +0200

    inserito titolo e immagine con vue js

---
+++ script.js
@@ -0,0 +1,11 @@
+const root = new Vue({
+    el: "#root",
+    data: {
+        title: "ciao Vue Js",
+        image: "https://www.tomshw.it/images/images/2020/03/disney-80005.jpg",
+        description: "baby yoda",
+    },
+    methods: {
+        
+    }
+})
